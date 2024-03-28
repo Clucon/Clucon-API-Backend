d74f2bdd3794d2ca10690f147355a7c0be71b420
0xaplus <codewithaplus@gmail.com>
2024-03-28 06:36:41 +0100

    first commit

---
+++ controllers/user.js
@@ -0,0 +1,215 @@
+const User = require("../models/user");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const jwt = require("jsonwebtoken");
+const sendToken = require("../utils/jwtToken");
+const sendMail = require("../utils/sendMail");
+
+// create user
+exports.createUser = async (req, res) => {
+  try {
+    const userData = req.body;
+    const userEmail = await User.findOne({ email: userData.email });
+
+    if (userEmail) {
+      return res.status(400).send("User already exists");
+    }
+
+    const user = {
+      ...userData,
+    };
+
+    const activationToken = createActivationToken(user);
+
+    const activationUrl = `http://localhost:3000/user/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email: user?.email,
+        subject: "Activate your account",
+        message: `Hello ${user.first_name}, please click on the link to activate your account: ${activationUrl}`,
+      });
+
+      res.status(201).json({
+        success: true,
+        message: `please check your email:- ${user.email} to activate your account!`,
+      });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+// create activation token
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
+
+// activate user
+exports.activateUser = catchAsyncErrors(async (req, res) => {
+  try {
+    // const { activation_token } = req.body;
+    const activation_token = req.params.token;
+
+    const newUser = jwt.verify(activation_token, process.env.ACTIVATION_SECRET);
+
+    if (!newUser) {
+      return res.status(400).send("Invalid token");
+    }
+    const userData = newUser;
+
+    let user = await User.findOne({ email: userData.email });
+
+    if (user) {
+      return res.status(400).send("User already exists");
+    }
+    user = await User.create({
+      ...userData,
+    });
+
+    sendToken(user, 201, res);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// login user
+exports.loginUser = catchAsyncErrors(async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).send("Please provide the all fields!");
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status().send("User doesn't exists!");
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      res.status(400).send({ message: "Please provide the correct information" });
+      return;
+    }
+
+    sendToken(user, 201, res);
+    res.status(200).send({ message: "Login successful." });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// load user
+exports.loadUser = catchAsyncErrors(async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(400).send({message: "User doesn't exists"});
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// log out user
+exports.logoutUser = catchAsyncErrors(async (req, res) => {
+  try {
+    res.cookie("token", null, {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    });
+    res.status(201).json({
+      success: true,
+      message: "Log out successful!",
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// update user info
+exports.updateUserInfo = catchAsyncErrors(async (req, res) => {
+  try {
+    const {
+      email,
+      password, //Existing password
+      first_name,
+      last_name,
+      phoneNo,
+    } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).send({message: "User not found"});
+    }
+
+    const isPasswordValid = await user.comparePassword(password);
+
+    if (!isPasswordValid) {
+      return next(
+        new ErrorHandler("Please provide the correct information", 400)
+      );
+    }
+
+    user.email = email;
+    user.password = password;
+    user.first_name = first_name;
+    user.last_name = last_name;
+    user.phoneNo = phoneNo;
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// update user password
+exports.forgotPassword = catchAsyncErrors(async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password updated successfully!",
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+// find user information with the userId
+exports.getUserById = catchAsyncErrors(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(201).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
---
+++ models/user.js
@@ -0,0 +1,77 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      minLength: 3,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      minLength: 6,
+      required: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        "Please enter a valid email address!",
+      ],
+    },
+    password: {
+      type: String,
+      minlength: [6, "Invalid length! Minimum is 6 characters"],
+    },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role",
+    },
+    phoneNo: {
+      type: String,
+      minLength: 10,
+      maxLength: 15,
+      unique: true,
+      required: true,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpires: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+// Hash password before saving user
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+  next();
+});
+
+// jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+// compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
